e1b81f498d3cc518980b34d147b98d0febfd1bb3
chicken0fwar <liamcwang@gmail.com>
2024-04-28 17:35:33 -0400

    bitmask idea

--- scripts/CollectionsUtil.cs
+++ scripts/CollectionsUtil.cs
@@ -12,7 +12,62 @@ namespace CollectionsUtil {
             return Enum.GetNames(someEnum).Length;
        }
     }
+
+    public class BitMask {
+        public Int32 value = 1;
+        private string[] names = new string[32];
+
+
+        public BitMask() {
+            names[0] = "None";
+            names[1] = "Default";
+        }
+
+        public void SetBit(int bit, string name) {
+            if (bit < 1) {
+                throw new ArgumentException("Bit given lower than 2, must not ovewrite preset values");
+            }
+            if (bit > 31) {
+                throw new ArgumentException("Bit given greater than 31.");
+            }
+
+            Int32 newBit = 1 << bit;
+            names[bit] = name;
+            value = value | newBit;
+        }
+
+        public void ClearBit(int bit) {
+            if (bit < 1) {
+                throw new ArgumentException("Bit given lower than 2, must not ovewrite preset values");
+            }
+            if (bit > 31) {
+                throw new ArgumentException("Bit given greater than 31.");
+            }
+
+            Int32 newBit = ~(1 << bit);
+            names[bit] = "";
+            value = value | newBit;
+        }
+
+        public bool NameInMask(string name) {
+            for (int i = 0; i < names.Length; i++) {
+                if (names[i] == name) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Int32 NameToBit(string name) {
+            for (int i = 0; i < names.Length; i++) {
+                if (names[i] == name) {
+                    return 1 << i;
+                }
+            }
+            throw new ArgumentException($"{name} not found in bitmask");
+        }
     }
+}
 
     
     
